833ceea623e5080cdcf13971b7dfe6907ebfa42e
kallard <kallard@student.21-school.ru>
2021-01-16 21:44:53 +0300

    main fixed

--- d05/ex02/main.cpp
+++ d05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kallard <kallard@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 11:32:09 by kallard           #+#    #+#             */
-/*   Updated: 2021/01/16 21:38:05 by kallard          ###   ########.fr       */
+/*   Updated: 2021/01/16 21:44:06 by kallard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,25 +21,25 @@
 
 int main()
 {
-	PresidentialPardonForm PPF("Bob");		//sign 25, exec 5
-	RobotomyRequestForm RRF("bureaucracy");	//sign 72, exec 45
 	ShrubberyCreationForm SCF("trees");		//sign 145, exec 137
+	RobotomyRequestForm RRF("bureaucracy");	//sign 72, exec 45
+	PresidentialPardonForm PPF("Bob");		//sign 25, exec 5
 
 	try {
 		std::cout << "Creating bureaucrat Sally with grade " << CLERK_GRADE << " . . ." << std::endl;		
 		Bureaucrat Sally("Sally", CLERK_GRADE);
 		std::cout << "SUCCESS!\n" << Sally;
 
-			std::cout << "Trying to sign all forms . . .\n";		
-			Sally.signForm(PPF);
-			Sally.signForm(RRF);
+			std::cout << "Trying to sign all forms . . .\n";
 			Sally.signForm(SCF);
-			std::cout << "Trying to execute all forms . . .\n";		
-			Sally.executeForm(PPF);
+			Sally.signForm(RRF);
+			Sally.signForm(PPF);
+			std::cout << "Trying to execute all forms . . .\n";
+			Sally.executeForm(SCF);	
 			Sally.executeForm(RRF);
 			// for (int i = 0; i < 5; i++)
 			// 	Sally.executeForm(RRF);
-			Sally.executeForm(SCF);
+			Sally.executeForm(PPF);
 		return (0);
 	}
 	catch (std::exception &e) {
