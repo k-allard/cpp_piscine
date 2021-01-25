05bd3a6da161e76d2f38f61044ee0c35b11bb609
kallard <kallard@student.21-school.ru>
2021-01-25 23:45:23 +0300

    ex02 fixed

--- d08/ex02/mutantstack.cpp
+++ d08/ex02/mutantstack.cpp
@@ -1,6 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mutantstack.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kallard <kallard@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/25 23:35:41 by kallard           #+#    #+#             */
+/*   Updated: 2021/01/25 23:41:37 by kallard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "mutantstack.hpp"
 
 template<typename T>
+MutantStack<T>::MutantStack() : std::stack<T>() { }
+
+template<typename T>
+MutantStack<T>::MutantStack(const MutantStack & src) : std::stack<T>(src) { }
+	
+template<typename T>
+MutantStack<T>::~MutantStack() { }
+
+template<typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack<T> & rhs) {
+	this->c = rhs.c;
+	return *this;
+}
+	
+template<typename T>
 typename MutantStack<T>::iterator MutantStack<T>::begin() {
 	return (this->c.begin());
+}
+
+template<typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end() {
+	return (this->c.end());
+}
+
+template<typename T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rbegin() {
+	return (this->c.rbegin());
+}
+
+template<typename T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rend() {
+	return (this->c.rend());
 }
--- d08/ex02/mutantstack.hpp
+++ d08/ex02/mutantstack.hpp
@@ -1,42 +1,23 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 #include <stack>
-#include <deque>
-
 #include <iostream>
 
 template<typename T>
 class MutantStack : public std::stack<T> {
+	public:
+		MutantStack();
+		MutantStack(const MutantStack & src);
+		virtual ~MutantStack();
+		MutantStack &operator=(const MutantStack & rhs);
 
-public:
-	MutantStack() : std::stack<T>() {}
-	MutantStack(const MutantStack & src) : std::stack<T>(src) {}
-	virtual ~MutantStack() {}
-	MutantStack &operator=(const MutantStack & rhs) {
-		this->c = rhs.c;
-		return *this;
-	}
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
 
-	typedef typename std::stack<T>::container_type::iterator iterator;
-	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
-
-	// iterator begin() {
-		
-	// 	return (this->c.begin());
-	// }
-	iterator begin();
-
-	iterator end() {
-		return this->c.end();
-	}
-
-	reverse_iterator rbegin(void) {
-		return this->c.rbegin();
-	}
-
-	reverse_iterator rend(void) {
-		return this->c.rend();
-	}
+		iterator begin();
+		iterator end();
+		reverse_iterator rbegin();
+		reverse_iterator rend();
 };
 
 #include "mutantstack.cpp"
