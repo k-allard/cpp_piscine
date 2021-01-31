6fbea3a4a4e00a9e12fb34a6760ff623166f370c
kallard <kallard@student.21-school.ru>
2021-01-31 18:10:58 +0300

    started exam 05 preparation

---
+++ exam/ex00/Warlock.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+class Warlock
+{
+private:
+	std::string name;
+	std::string title;
+public:
+	Warlock();
+	~Warlock();
+	Warlock(Warlock const & src);
+	Warlock & operator=(Warlock const & rhs);
+
+	Warlock(std::string name, std::string title);
+
+	std::string const & getName() const ;
+	std::string const & getTitle() const ;
+
+	void setTitle(std::string const & t);
+	
+	void introduce() const;
+
+};
